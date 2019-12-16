afea61c38759b4cafc2e72d86b10702b26638056
Bradley Buchsbaum <bbuchsbaum@Donacobius-641.local>
2019-12-16 16:18:53 -0500

    func_scans fix

--- R/bids.R
+++ R/bids.R
@@ -162,7 +162,8 @@ participants.bids_project <- function (x, ...) {
 #' fs <- func_scans(bids_project(p), subid="sub-0[123]", run="0[123]")
 func_scans.bids_project <- function (x, subid="^sub-.*", task=".*", run = ".*", modality="bold", ...) {
   ret <- x$bids_tree$Get("type", filterFun = function(z) {
-    if (!is.null(z$type) && z$modality == modality && str_detect(z$name, subid)  && str_detect(z$name, task) && str_detect(z$run, run)) {
+    if (!is.null(z$type) && z$modality == modality && str_detect(z$name, subid)  && str_detect(z$name, task) && str_detect(z$run, run) && str_detect(z$suffix, ".nii(.gz)?$")) {
+      browser()
       TRUE
     } else {
       FALSE
