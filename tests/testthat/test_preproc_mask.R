ffe36ab31c5846dd567021eced8519950430a42b
bbuchsbaum <brad.buchsbaum@gmail.com>
2025-05-18 16:24:36 -0400

    Add threshold check to create_preproc_mask

---
+++ tests/testthat/test_preproc_mask.R
@@ -0,0 +1,20 @@
+context("preproc_mask")
+library(testthat)
+library(bidser)
+
+# Helper to check if phoneme_stripped dataset with fmriprep derivatives is available
+has_phoneme_data <- function() {
+  test_path <- system.file("extdata/phoneme_stripped/derivatives/fmriprep", package="bidser")
+  nchar(test_path) > 0 && dir.exists(test_path)
+}
+
+# Error if thresh out of range
+
+test_that("create_preproc_mask errors for invalid thresh", {
+  skip_if_not(has_phoneme_data(), "Phoneme dataset with fmriprep derivatives not available")
+
+  proj <- bids_project(system.file("extdata/phoneme_stripped", package="bidser"), fmriprep=TRUE)
+  expect_error(create_preproc_mask(proj, subid="1001", thresh=1.5), "thresh")
+  expect_error(create_preproc_mask(proj, subid="1001", thresh=-0.1), "thresh")
+})
+
