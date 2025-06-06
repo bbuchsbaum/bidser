810683f4ea18d8f14610d1cc721ca64d1fbe1843
bbuchsbaum <brad.buchsbaum@gmail.com>
2025-06-06 07:13:46 -0400

    Add tests for path helpers and compliance

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -31,3 +31,35 @@ test_that("key_match matcher works with various patterns", {
   expect_false(nullpattern_matcher(list(task = "something")))
 })
 
+# Tests for generate_bids_path
+library(data.tree)
+
+test_that("generate_bids_path constructs expected directories", {
+  p_raw <- bidser:::generate_bids_path("01", datatype = "func", fmriprep = FALSE)
+  expect_equal(p_raw, "sub-01/func")
+
+  p_prep <- bidser:::generate_bids_path("01", session = "test", datatype = "anat",
+                                       fmriprep = TRUE, prep_dir = "derivatives/mockprep")
+  expect_equal(p_prep, "derivatives/mockprep/sub-01/ses-test/anat")
+})
+
+# Tests for reconstruct_node_path
+
+test_that("reconstruct_node_path handles raw and derivative nodes", {
+  root <- Node$new("proj")
+  raw <- root$AddChild("raw")
+  sub <- raw$AddChild("sub-01")
+  func <- sub$AddChild("func")
+  leaf <- func$AddChild("file.nii.gz")
+  expect_equal(bidser:::reconstruct_node_path(leaf), "sub-01/func/file.nii.gz")
+
+  deriv <- root$AddChild("derivatives")$AddChild("fmriprep")
+  subd <- deriv$AddChild("sub-01")
+  funcd <- subd$AddChild("func")
+  leafd <- funcd$AddChild("file_preproc.nii.gz")
+  expect_equal(bidser:::reconstruct_node_path(leafd, prep_dir = "derivatives/fmriprep"),
+               "derivatives/fmriprep/sub-01/func/file_preproc.nii.gz")
+
+  root_file <- root$AddChild("dataset_description.json")
+  expect_equal(bidser:::reconstruct_node_path(root_file), "dataset_description.json")
+})
